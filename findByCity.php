61c1fa90b379da80a896d7c1eec301030cab9275
Benjamin Barnes <benjaminrbarnes@gmail.com>
2015-12-06 11:59:32 -0800

    Reroute fix

--- findByCity.php
+++ findByCity.php
@@ -7,7 +7,6 @@ $conn = mysqli_connect($server, $user, $pass, $dbname, $port)
 or die('Error connecting to MySQL server.');
 
 ?>
-<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">-->
 <html>
     <head>
         <title>Search By City</title>
